29313c7a8dfab376678910335416648a40e6942e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-31 13:25:47 -0700

    Secret Message Box, The Riddler ip, Robot in space ip

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -16,7 +16,20 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		String Bunny= "secret";
+	String thesecretMessage= JOptionPane.showInputDialog("What is the secret message");
+	//String password =JOptionPane.showInputDialog("What is the password");
+	String guest = JOptionPane.showInputDialog("Do you know the password");
+	if (Bunny.equals(guest)){
 	
+	
+	JOptionPane.showMessageDialog(null,thesecretMessage);}
+	else {
+		JOptionPane.showMessageDialog(null,"Hacker!!!");
+	}
+	
+	}
 	// 1. Set a password in a String variable
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,11 +10,16 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+String answer= JOptionPane.showInputDialog("what type of tree have fingers");
+	// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (answer.equals("plam trees") {JOptionPane.showMessageDialog(null,"you got it correct");
+}
+else {
+	JOptionPane.showMessageDialog(null, "you got it wrong");
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
