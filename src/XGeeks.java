452a5d687b8d35c061b3b5f827badab64a168d05
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-14 13:26:10 -0700

    Banana4Eva, MyAge, XGeeks

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -13,7 +13,23 @@ import javax.swing.JOptionPane;
 */
 public class XGeeks {
 public static void main(String[] args) {
-
+String reyna = "high jumps";
+String Ayden = "laser eyes";
+String Jacob = "speaking to animals";
+String Tam = "power of cats";
+String name = JOptionPane.showInputDialog("Enter a name of someone on earth");
+if(name.equals("jacob")) {
+	JOptionPane.showMessageDialog(null, Jacob);
+}
+	if(name.equals("reyna")) {
+		JOptionPane.showMessageDialog(null, reyna);
+	}
+	if(name.equals("Ayden")) {
+		JOptionPane.showMessageDialog(null, Ayden);
+}
+	if(name.equals("Tam")) {
+	JOptionPane.showMessageDialog(null, Tam);
+}
 		// 1. Save the superpower for each person in a variable.
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
